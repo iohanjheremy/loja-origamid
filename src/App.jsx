2e9db0cbb15f345b89cbfaab61188972d9ea176f
Iohan Jheremy <iohanjheremy11@gmail.com>
2024-12-28 14:06:04 -0300

    Subindo todo o projeto

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Produto from './Produto'
+import './App.css'
+import './index.css'
+
+
+const App = () => {
+  const [dados, setDados] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+
+  async function handleClick(event){
+    setLoading(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`
+    );
+    const json = await response.json();
+    setDados(json);
+    setLoading(false);
+  }
+
+  return(
+    <div>
+      <h1>Lojinha dos Cria</h1>
+      <button style={{margin: '.5rem'}} onClick={handleClick}>Notebook</button>
+      <button style={{margin: '.5rem'}} onClick={handleClick}>Smartphone</button>
+      <button style={{margin: '.5rem'}} onClick={handleClick}>Tablet</button>
+
+      {loading && <p>Loading...</p>}
+      {!loading && dados && <Produto dados={dados} />}
+    </div>
+  )
+
+}
+
+export default App;
